8c45af9f0a2e5ef2f0257def23073826539da57c
manishaaaa24 <manisha12b0@gmail.com>
2025-12-02 13:26:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        print("Form Submitted Successfully")
+        print("Form Data: ", request.form)
+        return 'Registration Successful'
+    return render_template('register.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
